9ea75e62ddb9f523c20fb36f1c592d40640efa99
Kehinde Adeleke <adelekekehinde06@gmail.com>
2024-03-14 12:45:47 +0100

    initial commit and use clap

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+
+// in rust, it is customary to structure programs around the data 
+// they handle
+//
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+  pattern: String,
+  path: std::path::PathBuf
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
+}
+
