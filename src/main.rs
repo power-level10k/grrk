cfa7b987569c9dcf729cf1e8b4a06a0fe42e1cc2
Kehinde Adeleke <adelekekehinde06@gmail.com>
2024-03-16 00:18:26 +0100

    feat: handle error

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 
 // in rust, it is customary to structure programs around the data 
 // they handle
-//
+// I wasn't able to use the BufReader to read the file from the cli
 use clap::Parser;
-
+use anyhow::{Context, Result};
+// Search for a pattern in a file and display the lines that contain it
 #[derive(Parser)]
 struct Cli {
+  // the pattern to look for
   pattern: String,
+  // the path to the file to read
   path: std::path::PathBuf
 }
 
-fn main() {
+fn main() -> Result<()> { 
     let args = Cli::parse();
     
-    let content = std::fs::read_to_string(&args.path).expect("Could not read file");
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
     
     for line in content.lines() {
       if line.contains(&args.pattern) {
         println!("{}", line);
       }
     }
+
+  Ok(())
 }
 
