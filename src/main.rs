fdba7ab27bf1fc797e36e408315d48f0fd94c431
Kehinde Adeleke <adelekekehinde06@gmail.com>
2024-03-17 19:25:44 +0100

    feat: rename grrs to grrk

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,28 @@ use anyhow::{Context, Result};
 // Search for a pattern in a file and display the lines that contain it
 #[derive(Parser)]
 struct Cli {
-  // the pattern to look for
+  /// the pattern to look for
   pattern: String,
-  // the path to the file to read
+  /// the path to the file to read
   path: std::path::PathBuf
 }
 
+
+// A couple of action points
+// Functions are testable when they are smaller
+// so since we cannot call the main function,
+// we break them down into smaller functions
 fn main() -> Result<()> { 
     let args = Cli::parse();
     
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
     
-    for line in content.lines() {
-      if line.contains(&args.pattern) {
-        println!("{}", line);
-      }
-    }
-
+    grrk::find_matches(&content, &args.pattern, &mut std::io::stdout());
   Ok(())
 }
+
+
+
+
 
