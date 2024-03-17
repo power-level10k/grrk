3f42f7912aba4caa1f7f5fc6607423be362fb5e0
Kehinde Adeleke <adelekekehinde06@gmail.com>
2024-03-17 19:24:56 +0100

    feat: add integration tests

---
+++ tests/cli.rs
@@ -0,0 +1,46 @@
+use assert_cmd::prelude::*;// Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+use assert_fs::prelude::*;
+//
+
+#[test]
+
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>>{
+    let mut cmd = Command::cargo_bin("grrk")?;
+
+    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>>{
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrk")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("A test\nAnother test"));
+    
+    Ok(())
+}
+
+
+#[test]
+fn find_empty_string() -> Result<(), Box<dyn std::error::Error>>{
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("Another test\nMore Content\nAnother text")?;
+
+    let mut cmd = Command::cargo_bin("grrk")?;
+    cmd.arg("").arg(file.path());
+    cmd.assert()
+            .success()
+            .stdout(predicate::str::contains("Another test\nMore Content\nAnother text"));
+    Ok(())
+}
